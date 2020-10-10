abb97c8a70db70eb96b0c2afbb32c1cb156f8246
soyounglee026 <71009039+soyounglee026@users.noreply.github.com>
2020-10-11 01:25:33 +0900

    06-2

--- main.c
+++ main.c
@@ -3,18 +3,29 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int startPrint()
+int sumTwo(int a, int b)
 {
-	int i;
-	for (i=0; i<10; i++)
-		printf("*");
+	return (a+b);
+}
+
+int square(int n)
+{
+	return (n*n);
+}
+
+int get_max(int x, int y)
+{
+	if (x > y)
+		return x;
+	else
+		return y;
 }
 
 int main(int argc, char *argv[]) {
 	
-	startPrint();
-	startPrint();
-	startPrint();
+	printf("sumTwo : %i\n", sumTwo(1, 2));
+	printf("square : %i\n", square(5));
+	printf("get_max : %i\n", get_max(10, 30));
 	
 	return 0;
 }
